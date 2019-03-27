1a02fac6679c7665f447d05fa6b632e8e522f9fb
luismarioram99 <luismarioram99@gmail.com>
2019-03-27 10:26:38 -0600

    Nuevos metodos, buscar y agregar

--- src/Logic/Control.java
+++ src/Logic/Control.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class Control {
     
+    //prohibición de instancia
+    
     private Control(){}
+    
+    //llamada para ordenar base de alumnos.
     
     public static void ordenar(ArrayList<Alumno> dbase){
     
@@ -12,12 +16,18 @@ public class Control {
     
     }
     
+    //Combina dos subarreglos adyacentes ordenados en un arreglo ordenado
+    
     private static void merge(ArrayList<Alumno> dbase, int inicio, int fin){
     
+        //declaración de variables
+        
         int size = fin - inicio + 1;
         
         int sizeA = size/2;
         int sizeB = size - sizeA;
+        
+        //arreglos auxiliares
         
         Alumno[] arrA = new Alumno[sizeA];
         Alumno[] arrB = new Alumno[sizeB];
@@ -28,6 +38,8 @@ public class Control {
         
         contA = contB = 0;
         contC = inicio;
+        
+        //llenado de arreglos auxiliares
         
         while(contA < sizeA){
         
@@ -51,6 +63,8 @@ public class Control {
         contA = contB = 0;
         contC = inicio;
         
+        //llenado de arreglo principal
+        
         while(contA < sizeA && contB < sizeB){
             
             dbase.remove(contC);
@@ -70,6 +84,8 @@ public class Control {
             }
         
         }
+        
+        //vaciado de arreglos auxiliares
         
         while(contA < sizeA){
         
@@ -93,6 +109,8 @@ public class Control {
         
     }
     
+    //ordenamiento RECURSIVO
+    
     private static void mergeSort(int inicio, int fin, ArrayList<Alumno> dbase){
         
         if(inicio > fin)
@@ -102,6 +120,11 @@ public class Control {
         
         int mid = (size/2) - 1;
         
+        //divide el arreglo en dos
+        //ordena esas dos mitades
+        //combina esas dos mitades}
+        //asi para todo subarreglo
+        
         mergeSort(inicio, mid, dbase);
         mergeSort(mid+1, fin, dbase);
         
@@ -109,6 +132,38 @@ public class Control {
     
     }
     
+    //busqueda binaria para carnet de alumno
     
+    public static int buscar(ArrayList<Alumno> dbase, String carnet){
+    
+        int inicio = 0, fin = dbase.size() - 1;
+        
+        int mid;
+        String key;
+        
+        while(inicio < fin){
+        
+            mid = (inicio + fin)/2;
+            key = dbase.get(mid).getCarnet();
+            
+            if(key.compareTo(carnet) < 0)
+                inicio = mid+1;
+            else if(key.compareTo(carnet) > 0)
+                fin = mid-1;
+            else 
+                return mid;
+        
+        }
+        
+        return -1;
+    }
+    
+    public static void agregar(Alumno nuevo, ArrayList<Alumno> dbase){
+    
+        dbase.add(nuevo);
+        
+        ordenar(dbase);
+        
+    }
     
 }
