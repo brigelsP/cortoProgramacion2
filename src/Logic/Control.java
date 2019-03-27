7accd49109feee1b31ba35fec4b9bfba09da600f
luismarioram99 <luismarioram99@gmail.com>
2019-03-26 22:20:04 -0600

    cambios menores en clase control

--- src/Logic/Control.java
+++ src/Logic/Control.java
@@ -1,13 +1,80 @@
 package Logic;
 
+import java.util.ArrayList;
+
 public class Control {
     
     private Control(){}
     
-    public static ordenar(Arraylist alumnos){
+    public static void ordenar(ArrayList<Alumno> dbase){
     
         
     
+    }
+    
+    private static void Merge(ArrayList<Alumno> dbase, int inicio, int fin){
+    
+        int size = fin - inicio + 1;
+        
+        int sizeA = size/2;
+        int sizeB = size - sizeA;
+        
+        Alumno[] arrA = new Alumno[sizeA];
+        Alumno[] arrB = new Alumno[sizeB];
+        
+        int contA;
+        int contB;
+        int contC;
+        
+        contA = contB = 0;
+        contC = inicio;
+        
+        while(contA < sizeA){
+        
+            arrA[contA] = dbase.get(contC);
+            
+            contA ++;
+            contC ++;
+        
+        }
+        
+        while(contB < sizeB){
+        
+            arrB[contB] = dbase.get(contC);
+            
+            contB ++;
+            contC ++;
+        
+        }
+        
+        
+        contA = contB = 0;
+        contC = inicio;
+        
+        while(contA < sizeA && contB < sizeB){
+            
+            dbase.remove(contC);
+            
+            if(Alumno.compare(arrA[contA],arrB[contB]) < 0){            
+                
+                dbase.add(contC,arrA[contA]);
+                
+                contA++;
+            
+            }else{
+                
+                dbase.add(contC,arrB[contB]);
+                
+                contB++;
+            
+            }
+        
+        }
+        
+        
+        
+        
+        
     }
     
 }
